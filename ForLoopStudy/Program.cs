a8c24fb8d4233ad5be7cafbf02c3dea024680fce
DEVZONEYDS\USER <kwanwoo5@naver.com>
2017-12-26 19:36:55 +0900

    first commit

---
+++ ForLoopStudy/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoopStudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            int sum = 0;
+            for (;;) {
+                n++;
+                sum += n;
+                if (n > 2) break;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
